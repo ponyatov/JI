4a3979cbac1f6d8323894d7e4fd4ff5ce354e753
Dmitry Ponyatov <dponyatov@gmail.com>
2017-09-01 11:23:45 +0400

    + VM.py / stack
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ VM.py
@@ -0,0 +1,16 @@
+
+class Stack:
+    def __init__(self,init=None):
+        if init: self.dat=init
+        else: self.dat=[]
+    def push(self,o): self.dat.append(o); return self
+    def __iadd__(self,o): return self.push(o)
+    def pop(self): return self.dat.pop()
+    def __repr__(self):
+        S='[ '
+        for i in self.dat: S += '%s '%i
+        return S+']'
+
+def test_init(): assert Stack([1,2,3]).dat==[1,2,3] 
+def test_push(): s= Stack([1,2,3]) ; s += 4 ; assert s.dat == [1,2,3,4]
+def test_pop(): assert Stack([1,2,3]).pop() == 3
