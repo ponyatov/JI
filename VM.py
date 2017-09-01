d35461c37e27feefb879f95625136e83e9e5ba98
Dmitry Ponyatov <dponyatov@gmail.com>
2017-09-01 11:34:00 +0400

    + stack/pop
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- VM.py
+++ VM.py
@@ -7,6 +7,7 @@ class Stack:
     def push(self,o): self.dat.append(o); return self
     def __iadd__(self,o): return self.push(o)
     def pop(self): return self.dat.pop()
+    def top(self): return self.dat[-1]
     def __repr__(self):
         S='[ '
         for i in self.dat: S += '%s '%i
@@ -15,6 +16,7 @@ class Stack:
 def test_init(): assert Stack([1,2,3]).dat==[1,2,3] 
 def test_push(): s= Stack([1,2,3]) ; s += 4 ; assert s.dat == [1,2,3,4]
 def test_pop(): assert Stack([1,2,3]).pop() == 3
+def test_top(): assert Stack([1,2,3]).top() == 3
 
 ######### Machine ########
 
